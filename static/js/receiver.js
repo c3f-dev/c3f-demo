547e0b8b4856ec27d8699379b3c216ba10448e42
c3f-dev <c3f.dev@gmail.com>
2024-09-04 17:01:18 -0400

    Update receiver.js

--- static/js/receiver.js
+++ static/js/receiver.js
@@ -1,7 +1,3 @@
-//http://127.0.0.1:5500/deploy/receiver.html?id=LJH&browser=9ed987966c06808b50f9fddc24931bd1
-//http://127.0.0.1:5500/deploy/sender.html?id=LJH&browser=9ed987966c06808b50f9fddc24931bd1
-//Firefox:  ?id=LJH&browser=d6a5c9544eca9b5ce2266d1c34a93222
-//Opera:  ?id=LJH&browser=9ed987966c06808b50f9fddc24931bd1
 
 var offscreenCan;
 var gl;
